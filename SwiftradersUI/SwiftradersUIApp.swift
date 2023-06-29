95dc0a9a788022a326f5dfaec3f0036b4ec7cb9e
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-06-29 10:59:52 +0200

    Initial Commit

---
+++ SwiftradersUI/SwiftradersUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftradersUIApp.swift
+//  SwiftradersUI
+//
+//  Created by Emanuel Seemann on 29.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftradersUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
