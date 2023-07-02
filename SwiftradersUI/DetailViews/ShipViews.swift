688517143bfc6d2d6c72284e17399654a80bdcec
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-07-02 21:29:23 +0200

    add ShipViews

--- SwiftradersUI/DetailViews/ShipViews.swift
+++ SwiftradersUI/DetailViews/ShipViews.swift
@@ -7,14 +7,32 @@
 
 import SwiftUI
 
-struct ShipViews: View {
+struct FullShipView: View {
+    var ship : Ship
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ShipNameView(ship: ship)
+        }
+    }
+}
+
+struct ShipNameView: View {
+    var ship : Ship
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(ship.registration.name) (\(ship.symbol))").fontWeight(.bold).font(.title)
+            HStack {
+                Text(ship.registration.role).font(.title2).fontWeight(.light)
+                Spacer()
+                Text(ship.nav.status).font(.title2).fontWeight(.medium)
+            }
+            Text(ship.nav.waypointSymbol)
+        }.fixedSize(horizontal: true, vertical: false)
     }
 }
 
 struct ShipViews_Previews: PreviewProvider {
     static var previews: some View {
-        ShipViews()
+        FullShipView(ship : Ship())
     }
 }
